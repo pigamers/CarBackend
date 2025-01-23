9206d39e8fdc6e94edc9e1d45d8e4faf51efb757
pigamers <pigamers1123@gmail.com>
2025-01-23 14:26:22 +0530

    cloudinary setup

---
+++ utils/cloudinary.js
@@ -0,0 +1,16 @@
+const cloudinary = require('cloudinary').v2;
+const dotenv = require('dotenv');
+const fs = require('fs');
+const path = require('path');
+
+// Load environment variables
+dotenv.config();
+
+// Configure Cloudinary with your credentials
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+module.exports = cloudinary;
