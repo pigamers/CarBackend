ced23b40f946de9f0596051be8b7c0255bf061a6
pigamers <pigamers1123@gmail.com>
2025-02-24 11:35:39 +0530

    middleware added at routes

--- routes/user.route.js
+++ routes/user.route.js
@@ -8,21 +8,27 @@ const {
     updateUserProfile,
     changePassword
 } = require('../controllers/userauth.controller');
+const {
+    verifyToken,
+    validateLogin,
+    validateSignup,
+    validatePassword
+} = require('../middleware/auth.middleware');
 
 // Route for user login
-router.post("/login", login);
+router.post("/login", validateLogin, login);
 
 // Route for user signup
-router.post("/signup", signup);
+router.post("/signup", validateSignup, signup);
 
 // Example route for getting user profile
-router.get("/profile", getUserProfile);
+router.get("/profile", verifyToken, getUserProfile);
 
 // route for updating profile
-router.put("/update-profile", updateUserProfile);
+router.put("/update-profile", verifyToken, updateUserProfile);
 
 // password reset route
-router.post("/reset-password", changePassword);
+router.post("/reset-password", verifyToken, validatePassword, changePassword);
 
 
 module.exports = router;
